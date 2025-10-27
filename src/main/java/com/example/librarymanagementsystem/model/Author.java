75f34c6a20e1e2b3632e294e656e84456f7effb1
IuliaGalan <iuliagalan2014@gmail.com>
2025-10-27 19:51:05 +0200

    Author Model

---
+++ src/main/java/com/example/librarymanagementsystem/model/Author.java
@@ -0,0 +1,49 @@
+package com.example.librarymanagementsystem.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Relația M:N: un autor poate scrie mai multe carti
+
+public class Author {
+    private String id;
+    private String name;
+    private List<BookDetails> books;
+
+    public Author(String id, String name) {
+        this.id = id;
+        this.name = name;
+        this.books = new ArrayList<>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<BookDetails> getBooks() {
+        return books;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //relație bidirectionala
+    public void addBook(BookDetails book) {
+        if (!books.contains(book)) {
+            books.add(book);
+            book.addAuthor(this);
+        }
+    }
+
+    public void removeBook(BookDetails book) {
+        if (books.contains(book)) {
+            books.remove(book);
+            book.removeAuthor(this);
+        }
+    }
+}
