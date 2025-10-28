2a51e7c477d3a44ba76a10a9b5a6c9c383b70697
IuliaGalan <iuliagalan2014@gmail.com>
2025-10-28 13:36:20 +0200

    crearea interfete

---
+++ src/main/java/com/example/LibraryManagementSystem/service/BaseServiceInterface.java
@@ -0,0 +1,19 @@
+package com.example.librarymanagementsystem.service;
+
+import com.example.librarymanagementsystem.repository.InMemoryBaseRepo;
+
+import java.util.List;
+
+public interface BaseServiceInterface<T> {
+
+    public void add(String id, T entity);
+
+    public T getById(String id);
+
+    public List<T> getAll();
+
+    public void update(String id, T entity);
+
+    public void delete(String id);
+}
+
