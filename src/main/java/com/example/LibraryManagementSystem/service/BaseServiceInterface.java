ccd17cfd1d9b0137116832d1d5d578627e81580b
Cezara <cezarapitu91@gmail.com>
2025-11-03 23:39:57 +0200

    Finalize feature-cezara before switching

--- src/main/java/com/example/LibraryManagementSystem/service/BaseServiceInterface.java
+++ src/main/java/com/example/LibraryManagementSystem/service/BaseServiceInterface.java
@@ -1,6 +1,6 @@
 package com.example.LibraryManagementSystem.service;
 
-import com.example.librarymanagementsystem.repository.InMemoryBaseRepo;
+//import com.example.librarymanagementsystem.repository.InMemoryBaseRepo;
 
 import java.util.List;
 
